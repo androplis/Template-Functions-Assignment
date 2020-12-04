c07d9fedc86d2d04383feef231150efe189627b9
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-03 19:55:13 -0500

    Initial Commit

---
+++ Template Functions Assignment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Template Functions Assignment
+//
+//  Created by Andrew Biddle on 12/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
