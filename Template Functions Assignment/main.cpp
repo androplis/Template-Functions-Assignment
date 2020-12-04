5a7629ae1baacfc9d9ce2a33c6dab896366777ac
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-03 20:23:38 -0500

    Added template functions

--- Template Functions Assignment/main.cpp
+++ Template Functions Assignment/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  Template Functions Assignment
-//
-//  Created by Andrew Biddle on 12/3/20.
-//
+// Andrew Biddle
+// CIS 1202.501
+// 12.3.20
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    cout << endl << endl;
     return 0;
+}
+
+template<class T>
+T half(T value) {
+    return value / 2.0;
+}
+
+template<class T>
+T half(int value) {
+    return round(static_cast<double>(value / 2.0));
 }
