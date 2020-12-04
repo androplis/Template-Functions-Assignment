3a6c0a668883b7c28127b10a33bd9c16e10f8905
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-03 20:47:41 -0500

    Fixed template functions and added main driver

--- Template Functions Assignment/main.cpp
+++ Template Functions Assignment/main.cpp
@@ -5,18 +5,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    cout << endl << endl;
-    return 0;
-}
-
 template<class T>
 T half(T value) {
     return value / 2.0;
 }
 
-template<class T>
-T half(int value) {
-    return round(static_cast<double>(value / 2.0));
+// Is this explicitly specifying the type?
+int half(int value) {
+    return round(static_cast<double>(value) / 2);
 }
+
+
+int main() {
+    double x = 7.0;
+    float y = 5.0f;
+    int z = 3;
+    
+    cout << half(x) << endl;
+    cout << half(y) << endl;
+    cout << half(z) << endl;
+
+    cout << endl << endl;
+    return 0;
+}
+
